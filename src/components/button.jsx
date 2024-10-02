27f4fcae81d5eea6adae9365d9630bcdcbc08e0f
Jupower38300 <julien.legrand@my-digital-school.org>
2024-10-02 13:34:13 +0200

    Co-authored-by: Bastian <ScuBix1@users.noreply.github.com> Co-authored-by: shannonbsn <shannonbsn@users.noreply.github.com> Co-authored-by: GvansaKob <GvansaKob@users.noreply.github.com>

---
+++ src/components/button.jsx
@@ -0,0 +1,11 @@
+export function buttonJouer({text}) {
+    return(
+        <button className="p-4 bg-blue-600 text-white rounded-8">Jouer</button>
+    )
+};
+
+export const buttonJoue = () => {
+    return(
+        <button>Jouer</button>
+    )
+}
