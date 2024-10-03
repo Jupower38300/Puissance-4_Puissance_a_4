de6f40e594b2d8662b9529bcbf868c81e3753ca7
ScuBix1 <bastian.monnin1@gmail.com>
2024-10-03 11:04:55 +0200

    creation et ajout du bouton radio pour le bot

--- src/pages/Signup.jsx
+++ src/pages/Signup.jsx
@@ -1,12 +1,16 @@
 import { ButtonDarkBlue, ButtonLightBlue } from "../components/button"
-import { InputText } from "../components/input"
+import { InputRadio, InputText } from "../components/input"
 import { TokenPlay } from "../components/tokens"
 
 export const Signup = () => {
     return(
         <div className="inscription flex flex-col gap-12 py-12 bg-[#5A9AEE] m-0 min-h-[100dvh] justify-center">
             <InputText className="w-[60vw] mx-auto" placeholder={'Entrez votre pseudo...'}/>
-            <div className="choice-token flex justify-center items-center gap-12 h-28">
+            
+            <div className="flex justify-center gap-2">
+                <InputRadio label={'Bot'} name={'bot'} id={'bot'}/>
+            </div>
+            <div className="choice-token flex justify-center items-center flex-wrap gap-12 min-h-28">
                 <TokenPlay className={'w-24 h-24 bg-[rgba(255,0,0,0.6)] cursor-pointer hover:w-28 hover:h-28 ease-in duration-150'}/>
                 <TokenPlay className={'w-24 h-24 bg-[rgba(0,0,255,0.6)] cursor-pointer hover:w-28 hover:h-28 ease-in duration-150'}/>
             </div>
