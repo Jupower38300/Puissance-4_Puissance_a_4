f5f47ba09832829095fa3eed23f56b0c4599616a
Shannon Besson <shannon0413@outlook.com>
2024-10-03 14:19:15 +0200

    creation page Home

--- src/components/button.jsx
+++ src/components/button.jsx
@@ -1,11 +1,17 @@
-export function ButtonDarkBlue({text, className, onClick}) {
-    return(
-        <button className={`p-4 bg-[#1F1442] text-white rounded-lg py-2 px-8 ${className?className:''}`} onClick={onClick?onClick:''}>{text?text:''}</button>
+export function ButtonDarkBlue({ text, className, onClick }) {
+    return (
+        <button className={`p-4 bg-[#1F1442] text-white rounded-lg py-2 px-8 ${className ? className : ''}`} onClick={onClick ? onClick : ''}>{text ? text : ''}</button>
     )
 };
 
-export const ButtonLightBlue = ({text, className, onClick}) => {
-    return(
-        <button className={`p-4 bg-[#5A9AEE] text-white rounded-lg py-2 px-8 ${className?className:''}`} onClick={onClick?onClick:''}>{text?text:''}</button>
+export const ButtonLightBlue = ({ text, className, onClick }) => {
+    return (
+        <button className={`p-4 bg-[#5A9AEE] text-white rounded-lg py-2 px-8 ${className ? className : ''}`} onClick={onClick ? onClick : ''}>{text ? text : ''}</button>
+    )
+}
+
+export const ButtonPlay = ({ text, className, onClick }) => {
+    return (
+        <button className={`p-4 bg-[#5A9AEE] text-white rounded-lg py-2 px-8 text-4xl font-custom href="#circlePath" flex justify-center items-center ${className ? className : ''}`} onClick={onClick ? onClick : ''}>{text ? text : ''}</button>
     )
 }
---
+++ src/pages/Home.js
@@ -0,0 +1,26 @@
+import React from 'react';
+import * as Button from '../components/button';
+
+function Home() {
+    return (
+        <div className="App bg-black flex justify-center items-center h-screen bg-violet-950 h-screen">
+            <div class='flex flex-col '>
+                <h1 className='text-8xl text-white flex justify-center items-center h-screen font-custom href="#circlePath"'><span class="inline-block rotate-[15deg]"></span>
+                    <span class="inline-block rotate-[-35deg]">P</span>
+                    <span class="inline-block rotate-[-27deg] pb-[8%] pl-4">U</span>
+                    <span class="inline-block rotate-[-23deg] pb-[15%] pl-0">I</span>
+                    <span class="inline-block rotate-[-15deg] pb-[20%] pl-0">S</span>
+                    <span class="inline-block rotate-[-5deg] pb-[23%]">S</span>
+                    <span class="inline-block rotate-[5deg] pb-[23%]">A</span>
+                    <span class="inline-block rotate-[10deg] pb-[21%]">N</span>
+                    <span class="inline-block rotate-[15deg] pb-[18%]">C</span>
+                    <span class="inline-block rotate-[25deg] pb-[13%]">E</span>
+                    <span class="inline-block rotate-[-5deg]" className='text-violet-950'>"</span>
+                    <span class="inline-block rotate-[30deg]">4</span></h1>
+                <Button.ButtonPlay text='Play' />
+            </div>
+        </div>
+    );
+}
+
+export default Home;
